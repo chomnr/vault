d75a8064cb2262cadac620fc1d513526dfced35f
chomnr <zeljkovranjes@outlook.com>
2024-10-01 13:03:59 -0400

    env to const

---
+++ config/general.ts
@@ -0,0 +1,24 @@
+
+/* =======================================
+   ENV
+   ======================================= */
+
+export const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+
+export const VAULT_LIMIT = process.env.VAULT_LIMIT
+export const VAULT_CREDENTIAL_LIMIT = process.env.VAULT_CREDENTIAL_LIMIT
+
+export const COOKIE_SECRET = process.env.COOKIE_SECRET;
+
+export const MONGODB_URI = process.env.MONGODB_URI;
+
+export const ELASTICSEARCH_URL = process.env.ELASTICSEARCH_URL;
+export const ELASTICSEARCH_USERNAME = process.env.ELASTICSEARCH_USERNAME;
+export const ELASTICSEARCH_PASSWORD = process.env.ELASTICSEARCH_PASSSWORD;
+
+export const HOST_URL = process.env.URL;
+export const NODE_ENV = process.env.NODE_ENV;
+export const PORT = process.env.PORT;
+
+
