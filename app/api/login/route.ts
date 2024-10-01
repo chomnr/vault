978ef35f8293887193640989234fecc0636f53d0
chomnr <zeljkovranjes@outlook.com>
2024-10-01 14:08:09 -0400

    added api login route

---
+++ app/api/login/route.ts
@@ -0,0 +1,39 @@
+import { USERNAME_MAX_LENGTH, USERNAME_MIN_LENGTH } from "@/config/general"
+import { CRED_BAD_USERNAME, CRED_INCORRECT, CRED_REQUIRED } from "@/config/response"
+import { SessionData, SESSION_OPTIONS } from "@/config/session"
+import { err_route } from "@/config/shorthand"
+import { getIronSession } from "iron-session"
+import { cookies } from "next/headers"
+import { NextResponse } from "next/server"
+
+export async function POST(request: Request) {
+    try {
+        const data = await request.json()
+        const env = process.env
+        const { username, password } = data
+        if (!username || !password)
+            return err_route(CRED_REQUIRED.status,
+                CRED_REQUIRED.msg,
+                CRED_REQUIRED.code)
+        if ((username as string).length < USERNAME_MIN_LENGTH || 
+            (username as string).length > USERNAME_MAX_LENGTH)
+            return err_route(CRED_BAD_USERNAME.status,
+                CRED_BAD_USERNAME.msg,
+                CRED_BAD_USERNAME.code)
+        const { ADMIN_USERNAME, ADMIN_PASSWORD } = env
+        if (username !== ADMIN_USERNAME || password !== ADMIN_PASSWORD)
+            return err_route(CRED_INCORRECT.status,
+                CRED_INCORRECT.msg,
+                CRED_INCORRECT.code)
+        const session = await getIronSession<SessionData>(cookies(), SESSION_OPTIONS)
+        session.timeStamp = Date.now()
+        session.aesKey = undefined
+        session.currentVault = undefined
+        await session.save()
+        return new NextResponse(null, { status: 200 })
+    } catch {
+        return err_route(CRED_REQUIRED.status,
+            CRED_REQUIRED.msg,
+            CRED_REQUIRED.code)
+    }
+}
