ca94ee9fd92dc47e98b2abb4f215c55b60689a59
chomnr <zeljkovranjes@outlook.com>
2024-10-01 14:08:16 -0400

    added login error responses

---
+++ config/response.ts
@@ -0,0 +1,29 @@
+import { USERNAME_MAX_LENGTH, USERNAME_MIN_LENGTH } from "./general"
+
+interface ResponseError {
+    status: number,
+    msg: string,
+    code: string
+}
+
+/* =======================================
+   LOGIN ERROR
+   ======================================= */
+
+export const CRED_REQUIRED: ResponseError = {
+    status: 400,
+    msg: "Username and password are required",
+    code: "ERR_MISSING_CREDENTIALS"
+}
+
+export const CRED_INCORRECT: ResponseError = {
+    status: 401,
+    msg: "The account either does not exist or the password is incorrect",
+    code: "ERR_INCORRECT_CREDENTIALS"
+}
+
+export const CRED_BAD_USERNAME: ResponseError = {
+    status: 400,
+    msg: `The username must be between ${USERNAME_MIN_LENGTH} and ${USERNAME_MAX_LENGTH} characters long`,
+    code: "CRED_BAD_USERNAME"
+}
