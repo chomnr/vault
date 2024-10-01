93f71cfbaf4e9206fd7c8d8833c366a8237346f3
chomnr <zeljkovranjes@outlook.com>
2024-10-01 13:05:17 -0400

    added cookie settings

--- config/general.ts
+++ config/general.ts
@@ -3,22 +3,27 @@
    ENV
    ======================================= */
 
-export const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
-export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+export const ADMIN_USERNAME = process.env.ADMIN_USERNAME
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD
 
 export const VAULT_LIMIT = process.env.VAULT_LIMIT
 export const VAULT_CREDENTIAL_LIMIT = process.env.VAULT_CREDENTIAL_LIMIT
 
-export const COOKIE_SECRET = process.env.COOKIE_SECRET;
+export const COOKIE_SECRET = process.env.COOKIE_SECRET
 
-export const MONGODB_URI = process.env.MONGODB_URI;
+export const MONGODB_URI = process.env.MONGODB_URI
 
-export const ELASTICSEARCH_URL = process.env.ELASTICSEARCH_URL;
-export const ELASTICSEARCH_USERNAME = process.env.ELASTICSEARCH_USERNAME;
-export const ELASTICSEARCH_PASSWORD = process.env.ELASTICSEARCH_PASSSWORD;
+export const ELASTICSEARCH_URL = process.env.ELASTICSEARCH_URL
+export const ELASTICSEARCH_USERNAME = process.env.ELASTICSEARCH_USERNAME
+export const ELASTICSEARCH_PASSWORD = process.env.ELASTICSEARCH_PASSSWORD
 
-export const HOST_URL = process.env.URL;
-export const NODE_ENV = process.env.NODE_ENV;
-export const PORT = process.env.PORT;
+export const HOST_URL = process.env.URL
+export const NODE_ENV = process.env.NODE_ENV
+export const PORT = process.env.PORT
 
+/* =======================================
+   COOKIE
+   ======================================= */
 
+export const COOKIE_NAME = "session"
+export const COOKIE_MAX_AGE = 24 * 60 * 60
