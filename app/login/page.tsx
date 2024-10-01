7d9a666c765f3f98222ddc03046e296d270e23e7
chomnr <zeljkovranjes@outlook.com>
2024-10-01 12:37:16 -0400

    added checkbox

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,9 +9,15 @@ export default function Login() {
             <main className={styles.main}>
 
                 <div className="login">
+                    <h2>Sign In</h2>
+                    <p>Login to access your vault(s)</p>
                     <form>
                         <Input type="username" placeholder="Username" />
                         <Input type="password" placeholder="Password" />
+                        <div className="checkbox">
+                            <Input type="checkbox" placeholder="Password" />
+                            Remember me
+                        </div>
                         <button>Login</button>
                     </form>
                 </div>
