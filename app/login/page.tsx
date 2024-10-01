d13724cd086e52766d8b68b7fdc5c8560267b444
chomnr <zeljkovranjes@outlook.com>
2024-10-01 03:21:06 -0400

    fixed login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,12 @@
 import "@/styles/custom.css";
+import styles from "../page.module.css";
 
 export default function Login() {
-    return (<div></div>)
+    return (
+        <div className={styles.page}>
+            <main className={styles.main}>
+                asddsasd
+            </main>
+        </div>
+    )
 }
