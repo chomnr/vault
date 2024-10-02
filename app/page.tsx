1178ab79fc752214ec3538a4520e6a0ff6b7b937
chomnr <zeljkovranjes@outlook.com>
2024-10-02 13:45:52 -0400

    styled vault and added hover

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
+'use client'
+
+import { useEffect, useRef } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const vaultsRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    if (vaultsRef.current) {
+      const vaults = vaultsRef.current;
+      const children = vaults.children;
+      for (let i = 0; i < children.length; i++) {
+        const vault = children[i] as HTMLDivElement;
+        vault.addEventListener('mouseover', (e) => {
+          (vault.children[0] as HTMLDivElement).style.borderColor = 'var(--vault-individual-hover-border)';
+        })
+        vault.addEventListener('mouseout', (e) => {
+          (vault.children[0] as HTMLDivElement).style.borderColor = 'var(--vault-individual-border)';
+        })
+      }
+    }
+  }, []);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div className="vaults">
-          dsdsdsadsdasds
+        <div ref={vaultsRef} className="vaults">
+          <div className="vault">
+            <div className="inner">
+              <div className="icon">1</div>
+            </div>
+            <div className="name">VAULT ONE</div>
+          </div>
         </div>
       </main>
     </div>
