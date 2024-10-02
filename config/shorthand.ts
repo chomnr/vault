73fd07c8682f6b93f645af7525a2fefd583eb3a7
chomnr <zeljkovranjes@outlook.com>
2024-10-02 00:24:24 -0400

    removed import

--- config/shorthand.ts
+++ config/shorthand.ts
@@ -1,7 +1,6 @@
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import { SessionData, SESSION_OPTIONS } from "./session";
 
 export const err_route = (status: number, error: string, code: string) => NextResponse.json({ status: status, error: error, code: code, timestamp: new Date().toISOString() }, { status })
 export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
