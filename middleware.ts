99a5a273bd5d6136a8c802f8915d094acc8e2679
chomnr <zeljkovranjes@outlook.com>
2024-10-02 16:58:56 -0400

    removed debug code

--- middleware.ts
+++ middleware.ts
@@ -18,8 +18,6 @@ export async function middleware(request: NextRequest) {
             return NextResponse.redirect(new URL('/', request.url))
         }
     }
-
-    console.log(request.nextUrl.pathname)
     return result
 }
 
