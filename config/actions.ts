c381ea0af851fda11a04fe4cd491a8d290b452ab
chomnr <zeljkovranjes@outlook.com>
2024-10-01 20:40:55 -0400

    removed ;

--- config/actions.ts
+++ config/actions.ts
@@ -3,19 +3,26 @@
 import { redirect } from 'next/navigation'
 import { HOST_URL } from './general'
 import { newSession, SESSION_OPTIONS, SessionData } from './session';
+import { getIronSession } from 'iron-session';
+import { cookies } from 'next/headers';
+
+export async function getSession() {
+    //const session = await getIronSession<SessionData>(cookies(), SESSION_OPTIONS)
+    
+}
 
 export async function login(prevState: { result: { error: any; code: any; timestamp: any; } }, form: FormData) {
     const { username, password, remember } = {
         username: form.get('username') as string,
         password: form.get('password') as string,
         remember: form.get('remember') === 'on'
-    };
+    }
     const response = await fetch(HOST_URL + "/api/login", {
         method: 'POST',
         credentials: 'include',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password, remember }),
-    });
+    })
     if (!response.ok) {
         const result = await response.json();
         return {
@@ -24,9 +31,9 @@ export async function login(prevState: { result: { error: any; code: any; timest
                 code: result.code,
                 timestamp: result.timestamp || new Date().toISOString(),
             }
-        };
+        }
     }
-    await newSession(new Uint8Array(), "", remember);
+    await newSession(new Uint8Array(), "", remember)
     redirect("/")
-    return { result: { error: null, code: null, timestamp: null } };
+    return { result: { error: null, code: null, timestamp: null } }
 }
