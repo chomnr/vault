7eb6d332efd95a7612a566010bc8f23e7883a563
chomnr <zeljkovranjes@outlook.com>
2024-10-01 20:36:32 -0400

    adjusted alert added code

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,11 +1,13 @@
 interface AlertProps {
-    type: 'danger' | 'success' | 'warning' | 'info' | 'secondary';
-    message: string;
+    type: 'danger' | 'success' | 'warning' | 'info' | 'secondary'
+    message: string
+    code: string
 }
 
-export const Alert: React.FC<AlertProps> = ({ type, message }) => {
+export const Alert: React.FC<AlertProps> = ({ type, message, code }) => {
     return (
         <div aria-live="polite" className={`alert ${type}`}>
+            <span className="code">{code}</span>
             {message}
         </div>
     );
