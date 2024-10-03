e4224013887f4dfcb84b09298a46cea233666cd2
chomnr <zeljkovranjes@outlook.com>
2024-10-03 00:05:58 -0400

    adjusted input more fields

--- components/input.tsx
+++ components/input.tsx
@@ -1,15 +1,19 @@
+import { LegacyRef } from "react"
+
 interface InputProps {
+    id?: string
     type: string
     name?: string
     placeholder?: string
     required?: boolean
     value?: string
-    onchange?: any,
+    onchange?: any
     disabled?: boolean
+    ref?: LegacyRef<any>
 }
 
-export const Input: React.FC<InputProps> = ({ type, name, placeholder, required, value, onchange, disabled }) => {
+export const Input: React.FC<InputProps> = ({ id, ref, type, name, placeholder, required, value, onchange, disabled }) => {
     return (
-        <input type={type} name={name} placeholder={placeholder} required={required} value={value} onChange={onchange} disabled={disabled}/>
+        <input id={id} ref={ref} type={type} name={name} placeholder={placeholder} required={required} value={value} onChange={onchange} disabled={disabled}/>
     );
 };
