7cd748bdd9104cd38426f310617549c6ef9b460e
chomnr <zeljkovranjes@outlook.com>
2024-10-03 00:05:53 -0400

    added checkmark icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -2,24 +2,29 @@ import { IconSvgProps } from "@/types";
 import * as React from "react";
 
 
-export const Logo: React.FC<IconSvgProps> = ({
-  size = 36,
-  width,
-  height,
+export const CheckMark: React.FC<IconSvgProps> = ({
+  size = 42,
+  width = size,
+  height = size,
   ...props
 }) => (
   <svg
-    fill="none"
-    height={size || height}
-    viewBox="0 0 32 32"
-    width={size || width}
+    fill="#ffffff"
+    width={width}
+    height={height}
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <path
-      clipRule="evenodd"
-      d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-      fill="currentColor"
-      fillRule="evenodd"
+    <polyline
+      points="5 12 10 17 19 8"
+      style={{
+        fill: 'none',
+        stroke: 'rgb(255, 255, 255)',
+        strokeLinecap: 'round',
+        strokeLinejoin: 'round',
+        strokeWidth: 2,
+      }}
     />
   </svg>
 );
