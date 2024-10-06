6463c69de6267ee0e67d2355020b0fae6b666e94
chomnr <zeljkovranjes@outlook.com>
2024-10-06 16:27:19 -0400

    added name length check

--- app/api/vaults/route.ts
+++ app/api/vaults/route.ts
@@ -1,11 +1,20 @@
+import { VAULT_NAME_MIN_LENGTH, VAULT_NAME_MAX_LENGTH } from "@/config/general";
+import { VAULT_BAD_NAME } from "@/config/response";
+import { err_route } from "@/config/shorthand";
 import { PrismaClient } from "@prisma/client/extension";
 
 // create vualt...
 export async function POST(request: Request) {
     const prisma = new PrismaClient()
     const data = await request.json()
-    const env = process.env
-    const { username, password, remember } = data
+    const { name, maxCredentials } = data
+    if ((name as string).length > VAULT_NAME_MIN_LENGTH &&
+        (name as string).length < VAULT_NAME_MAX_LENGTH) {
+            return err_route(VAULT_BAD_NAME.status,
+                VAULT_BAD_NAME.msg,
+                VAULT_BAD_NAME.code)
+    }
+    
     /*
     const vault = await prisma.vault.create({
         data: {
