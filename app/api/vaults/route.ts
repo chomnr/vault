af3b52d14f6d38c514f638dd95ae99308644d749
chomnr <zeljkovranjes@outlook.com>
2024-10-06 16:34:38 -0400

    added more checks

--- app/api/vaults/route.ts
+++ app/api/vaults/route.ts
@@ -1,5 +1,5 @@
-import { VAULT_NAME_MIN_LENGTH, VAULT_NAME_MAX_LENGTH } from "@/config/general";
-import { VAULT_BAD_NAME } from "@/config/response";
+import { VAULT_NAME_MIN_LENGTH, VAULT_NAME_MAX_LENGTH, VAULT_CREDENTIAL_LIMIT } from "@/config/general";
+import { VAULT_BAD_NAME, VAULT_CREDENTIALS_EXCEEDED, VAULT_MAX_CREDENTIALS_NOT_INTEGER } from "@/config/response";
 import { err_route } from "@/config/shorthand";
 import { PrismaClient } from "@prisma/client/extension";
 
@@ -14,7 +14,16 @@ export async function POST(request: Request) {
                 VAULT_BAD_NAME.msg,
                 VAULT_BAD_NAME.code)
     }
-    
+    if (typeof maxCredentials !== 'number' || isNaN(maxCredentials)) {
+        return err_route(VAULT_MAX_CREDENTIALS_NOT_INTEGER.status,
+            VAULT_MAX_CREDENTIALS_NOT_INTEGER.msg,
+            VAULT_MAX_CREDENTIALS_NOT_INTEGER.code)
+    }
+    if((maxCredentials as number) > VAULT_CREDENTIAL_LIMIT) {
+        return err_route(VAULT_CREDENTIALS_EXCEEDED.status,
+            VAULT_CREDENTIALS_EXCEEDED.msg,
+            VAULT_CREDENTIALS_EXCEEDED.code)
+    }
     /*
     const vault = await prisma.vault.create({
         data: {
--- config/response.ts
+++ config/response.ts
@@ -1,4 +1,4 @@
-import { USERNAME_MAX_LENGTH, USERNAME_MIN_LENGTH, VAULT_MAX_LENGTH, VAULT_MIN_LENGTH } from "./general"
+import { USERNAME_MAX_LENGTH, USERNAME_MIN_LENGTH, VAULT_CREDENTIAL_LIMIT, VAULT_NAME_MAX_LENGTH, VAULT_NAME_MIN_LENGTH } from "./general"
 
 interface ResponseError {
     status: number,
@@ -46,6 +46,18 @@ export const LOGIN_EXPIRED: ResponseError = {
    
 export const VAULT_BAD_NAME: ResponseError = {
     status: 400,
-    msg: `The vault name must be between ${VAULT_MIN_LENGTH} and ${VAULT_MAX_LENGTH} characters long`,
+    msg: `The vault name must be between ${VAULT_NAME_MIN_LENGTH} and ${VAULT_NAME_MAX_LENGTH} characters long`,
     code: "ERR_VAULT_BAD_NAME"
+}
+
+export const VAULT_MAX_CREDENTIALS_NOT_INTEGER: ResponseError = {
+    status: 400,
+    msg: `The max credentials value must be an integer`,
+    code: "ERR_VAULT_MAX_CREDENTIALS_NOT_INTEGER"
+}
+
+export const VAULT_CREDENTIALS_EXCEEDED: ResponseError = {
+    status: 400,
+    msg: `The number of credentials exceeds the maximum allowed for this vault ${VAULT_CREDENTIAL_LIMIT}`,
+    code: "ERR_VAULT_CREDENTIALS_EXCEEDED"
 }
