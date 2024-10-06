fbe45a5f6e4388a6491eabf03be5fd912f0f92a3
chomnr <zeljkovranjes@outlook.com>
2024-10-05 21:24:36 -0400

    removed ref and fixed it

--- components/input.tsx
+++ components/input.tsx
@@ -1,19 +1,30 @@
-import { LegacyRef } from "react"
+import React, { forwardRef } from 'react';
 
 interface InputProps {
-    id?: string
-    type: string
-    name?: string
-    placeholder?: string
-    required?: boolean
-    value?: string
-    onchange?: any
-    disabled?: boolean
-    ref?: LegacyRef<any>
+    id?: string;
+    type: string;
+    name?: string;
+    placeholder?: string;
+    required?: boolean;
+    value?: string | number;
+    onchange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    disabled?: boolean;
 }
 
-export const Input: React.FC<InputProps> = ({ id, ref, type, name, placeholder, required, value, onchange, disabled }) => {
-    return (
-        <input id={id} ref={ref} type={type} name={name} placeholder={placeholder} required={required} value={value} onChange={onchange} disabled={disabled}/>
-    );
-};
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+    ({ id, type, name, placeholder, required, value, onchange, disabled }, ref) => {
+        return (
+            <input
+                id={id}
+                ref={ref}
+                type={type}
+                name={name}
+                placeholder={placeholder}
+                required={required}
+                value={value}
+                onChange={onchange}
+                disabled={disabled}
+            />
+        );
+    }
+);
