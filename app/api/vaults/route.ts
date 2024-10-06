af23cc412442c36ee4b5f03b331bac987120cfe3
chomnr <zeljkovranjes@outlook.com>
2024-10-06 16:23:25 -0400

    basic testing with vault creation

--- app/api/vaults/route.ts
+++ app/api/vaults/route.ts
@@ -1,3 +1,20 @@
+import { PrismaClient } from "@prisma/client/extension";
+
+// create vualt...
 export async function POST(request: Request) {
-    // ....
+    const prisma = new PrismaClient()
+    const data = await request.json()
+    const env = process.env
+    const { username, password, remember } = data
+    /*
+    const vault = await prisma.vault.create({
+        data: {
+            name: "name..",
+            maxCredentials: 100,
+            credentials: {
+                create: []
+            }
+        }
+    })
+        */
 }
