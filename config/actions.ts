998155921a4007fd3b8f6656baf44cdd288d34d6
chomnr <zeljkovranjes@outlook.com>
2024-10-07 12:43:39 -0400

    using interface for vault now (SessionVault)

--- config/actions.ts
+++ config/actions.ts
@@ -39,7 +39,7 @@ export async function login(prevState: { result: { error: any; code: any; timest
             }
         }
     }
-    await newSession(new Uint8Array(), "", remember)
+    await newSession(new Uint8Array(), undefined, remember)
     return redirect("/")
 }
 
--- config/session.ts
+++ config/session.ts
@@ -1,12 +1,19 @@
 import { getIronSession, SessionOptions } from "iron-session";
 import { COOKIE_MAX_AGE, COOKIE_NAME, COOKIE_SECRET, NODE_ENV } from "./general";
 import { cookies } from "next/headers";
+import { Vault } from "@prisma/client";
 
 export interface SessionData {
     timeStamp: number,
     key: Uint8Array | undefined,
-    vault: string | undefined,
+    vault: SessionVault | undefined,
     remember: boolean
+}
+
+export interface SessionVault {
+    id: string | undefined,
+    name: string | undefined,
+    key: string | undefined
 }
 
 export const SESSION_OPTIONS: SessionOptions = {
@@ -18,7 +25,7 @@ export const SESSION_OPTIONS: SessionOptions = {
     },
 };
 
-export const newSession = async (key: Uint8Array, vault: string, remember: boolean) => {
+export const newSession = async (key: Uint8Array, vault: SessionVault | undefined, remember: boolean) => {
     const session = await getIronSession<SessionData>(cookies(), SESSION_OPTIONS)
     session.timeStamp = Date.now()
     session.key = key
