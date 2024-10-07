524647620f2135cdacde09adf20ac71e059e3d73
chomnr <zeljkovranjes@outlook.com>
2024-10-07 17:27:21 -0400

    added post to clear session link

---
+++ app/api/vaults/link/clear/route.ts
@@ -0,0 +1,14 @@
+import { LOGIN_REQUIRED } from "@/config/response"
+import { SessionData, SESSION_OPTIONS } from "@/config/session"
+import { err_route } from "@/config/shorthand"
+import { getIronSession } from "iron-session"
+import { cookies } from "next/headers"
+
+export async function POST(request: Request) {
+    const session = await getIronSession<SessionData>(cookies(), SESSION_OPTIONS)
+    if (Object.keys(session).length === 0)
+        return err_route(LOGIN_REQUIRED.status,
+            LOGIN_REQUIRED.msg,
+            LOGIN_REQUIRED.code)
+    session.vault = undefined
+}
