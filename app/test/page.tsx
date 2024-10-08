c6a90d3dcc5cb00e499fc5c80f1cbdd4766756dd
chomnr <zeljkovranjes@outlook.com>
2024-10-07 21:01:02 -0400

    testing

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,71 +1,36 @@
 'use client'
-import { useState } from "react";
-import { Alert } from "@/components/alert";
+
+import { useEffect, useRef, useState } from "react";
 import styles from "../page.module.css";
+import { decryptVault } from "@/config/actions";
+import { useFormState } from "react-dom";
+import { Alert } from "@/components/alert";
+import React from "react";
 
+const initialState = {
+  result: {
+    error: null,
+    code: null,
+    timestamp: null,
+    data: null
+  },
+};
 
-export default function Test() {
-  const [message, setMessage] = useState("YWRzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nz");
-  const [alertMessage, setAlertMessage] = useState("SUCCESS (CLICK TO COPY)");
-
-  const handleCopy = async (msg: string) => {
-    try {
-      await navigator.clipboard.writeText(msg);
-      console.log("Message copied to clipboard!");
-      setAlertMessage("COPIED");
-      setTimeout(() => setAlertMessage("SUCCESS (CLICK TO COPY)"), 2000);
-    } catch (err) {
-      console.error("Failed to copy message: ", err);
-    }
-  };
-
+export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        sddsadsadscdsasadads
-      </main>
-    </div>
-  );
-}
-/*
-
-<div className="flex-box">
-            <div id="1" className="vault">
-              <div className="inner">
-                <div className="icon">1</div>
-              </div>
-              <div className="name">VAULT ONE</div>
+        <div className="vault creation">
+          <h1>Vault</h1>
+          <div className="divider"></div>
+          <div className="flex-box row">
+            <div className="flex-box col" style={{ gap: '7px' }}>
+              <br/>
+              <p>To access this vault, you must upload the AES 256 encryption key that is bound to it. This key is required to view the vault's contents. Once inside the vault, each value must be manually decrypted using the provided key.</p>
             </div>
           </div>
-
-
-export default function Test() {
-  const [fileName, setFileName] = useState<string | null>(null);
-  const keyUploadRef = useRef<HTMLInputElement>(null)
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files ? e.target.files[0] : null;
-    setFileName(file ? file.name : null);
-    if (keyUploadRef.current) {
-      keyUploadRef.current.disabled = true
-    }
-  };
-  // show error if key is incorrect show proceed button if it's successful
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <form className="key-upload">
-          <Alert type={"danger"} code="ERR_BAD_KEY" message="The key you uploaded does not belong to the corresponding vault" />
-          <Alert type={"success"} code="Success" message="The key has successfully decrypted the contents you may proceed" />
-          <label htmlFor="key-upload" className="custom-file-upload">
-            {fileName ? fileName : 'Upload AES 256 Key'}
-          </label>
-          <Input id="key-upload" ref={keyUploadRef} name="key-upload" type={"file"} onchange={handleFileChange} />
-          {fileName ? <button className="submit">
-            Decrypt Vault
-          </button> : null}
-        </form>
+        </div>
       </main>
     </div>
   );
 }
-*/
