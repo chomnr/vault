800066c2181b9f79d3a5504f319cd43d77cf3ace
chomnr <zeljkovranjes@outlook.com>
2024-10-07 21:42:42 -0400

    added state loader

--- app/page.tsx
+++ app/page.tsx
@@ -2,82 +2,54 @@
 
 import { useEffect, useRef, useState } from "react";
 import styles from "./page.module.css";
-import { Input } from "@/components/input";
 
 export default function Home() {
   const vaultsRef = useRef<HTMLDivElement>(null);
-  const [vaultKeyUpload] = useState(false);
   const [vaults, setVaults] = useState([]);
-  const [fileName, setFileName] = useState<string | null>(null);
-  const keyUploadRef = useRef<HTMLInputElement>(null);
-  
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files ? e.target.files[0] : null;
-    setFileName(file ? file.name : null);
-  };
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    if (keyUploadRef.current) {
-      keyUploadRef.current.disabled = !!fileName;
-    }
-  }, [fileName]);
-
-  const handleVaultRetrieval = async () => {
+  const fetchVaults = async () => {
+    setLoading(true);
     try {
-      const response = await fetch('/api/vaults', {
-        method: 'GET',
-        credentials: 'include',
-      });
-      if (!response.ok) {
-        throw new Error('Failed to retrieve vaults');
-      }
-      const vaultsData = await response.json();
-      setVaults(vaultsData);
+      const response = await fetch('/api/vaults', { method: 'GET', credentials: 'include' });
+      if (!response.ok) throw new Error('Failed to retrieve vaults');
+      setVaults(await response.json());
     } catch (error) {
-      console.error('Error retrieving vaults:', error);
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
+
   useEffect(() => {
-    handleVaultRetrieval();
+    fetchVaults();
   }, []);
 
   const handleVaultLink = async (id: string) => {
     const response = await fetch('/api/vaults/link', {
       method: 'POST',
       credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ id: id }),
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id }),
     });
-    if (response.redirected) {
-      window.location.href = response.url;
-    }
+    if (response.redirected) window.location.href = response.url;
   };
 
   useEffect(() => {
-    const vaults = vaultsRef.current;
+    const vaults = vaultsRef.current?.children;
     if (vaults) {
-      const children = Array.from(vaults.children) as HTMLDivElement[];
-      children.forEach((vault) => {
-        const handleMouseOver = () => {
+      Array.from(vaults).forEach(vault => {
+        vault.addEventListener('mouseover', () => {
           (vault.children[0].children[0] as HTMLDivElement).style.color = 'var(--vault-individual-hover-color)';
-          if (vault.children[1] !== undefined) {
+
+          if (vault.children[1] !== undefined)
             (vault.children[1] as HTMLDivElement).style.color = 'var(--vault-individual-hover-border)';
-          }
-        };
-        const handleMouseOut = () => {
+        });
+        vault.addEventListener('mouseout', () => {
           (vault.children[0].children[0] as HTMLDivElement).style.color = 'var(--vault-individual-color)';
-          if (vault.children[1] !== undefined) {
+          if (vault.children[1])
             (vault.children[1] as HTMLDivElement).style.color = 'var(--vault-individual-color)';
-          }
-        };
-        vault.addEventListener('mouseover', handleMouseOver);
-        vault.addEventListener('mouseout', handleMouseOut);
-        return () => {
-          vault.removeEventListener('mouseover', handleMouseOver);
-          vault.removeEventListener('mouseout', handleMouseOut);
-        };
+        });
       });
     }
   }, [vaults]);
@@ -85,60 +57,31 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        {!vaultKeyUpload ? <></> : (null)}
-        {!vaultKeyUpload ? (
-          <>
-            <div className="vault header">{/* HEADER HERE */}</div>
-            <div ref={vaultsRef} className="vaults">
-              {vaults.length > 0 ? (
-                vaults.map((vault, index) => (
-                  <div
-                    key={vault['id']}
-                    id={vault['id']}
-                    className="vault"
-                    onClick={() => {
-                      handleVaultLink(vault['id']);
-                    }}
-                  >
-                    <div className="inner">
-                      <div className="icon">{index + 1}</div>
-                    </div>
-                    <div className="name">{vault['name']}</div>
+        <div className="vault header">{/* HEADER HERE */}</div>
+        {loading ? (
+          <div className="loading">
+            <div className="loader"></div>
+          </div>
+        ) : (
+          <div ref={vaultsRef} className="vaults">
+            {vaults.length > 0 ? (
+              vaults.map((vault, index) => (
+                <div key={vault["id"]} className="vault" onClick={() => handleVaultLink(vault["id"])}>
+                  <div className="inner">
+                    <div className="icon">{index + 1}</div>
                   </div>
-                ))
-              ) : (
-                null
-              )}
-
-              <div
-                id="vault_add"
-                className="vault"
-                onClick={() => window.location.href = "/vault/create"}
-              >
-                <div className="inner">
-                  <div className="icon">+</div>
+                  <div className="name">{vault["name"]}</div>
                 </div>
+              ))
+            ) : (
+              <div>No vaults available</div>
+            )}
+            <div id="vault_add" className="vault" onClick={() => window.location.href = "/vault/create"}>
+              <div className="inner">
+                <div className="icon">+</div>
               </div>
             </div>
-          </>
-        ) : (
-          <form className="key-upload">
-            <label htmlFor="key-upload" className="custom-file-upload">
-              {fileName ? fileName : 'Upload AES 256 Key'}
-            </label>
-            <Input
-              id="key-upload"
-              ref={keyUploadRef}
-              name="key-upload"
-              type={"file"}
-              onchange={handleFileChange}
-            />
-            {fileName ? (
-              <button className="submit">
-                Decrypt Vault
-              </button>
-            ) : null}
-          </form>
+          </div>
         )}
       </main>
     </div>
--- app/vault/decrypt/page.tsx
+++ app/vault/decrypt/page.tsx
@@ -21,11 +21,12 @@ export default function Home() {
     const keyUploadRef = useRef<HTMLInputElement>(null);
     const formRef = useRef<HTMLFormElement>(null);
     const [state, formAction] = useFormState(decryptVault, initialState);
+    const [vault, setVault] = useState<{ id: string; name: string } | null>(null);
+    const [loading, setLoading] = useState(true);
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files ? e.target.files[0] : null;
         setFileName(file ? file.name : null);
     };
-    const [vault, setVault] = useState<{ id: string; name: string } | null>(null);
     useEffect(() => {
         if (keyUploadRef.current) {
             keyUploadRef.current.disabled = !!fileName;
@@ -62,45 +63,55 @@ export default function Home() {
                     setVault(data);
                 }
             } catch (err) {
-                console.log(err)
+                console.log(err);
+            } finally {
+                setLoading(false); // End loading
             }
         };
         fetchVaultDetails();
     }, []);
+
     return (
         <div className={styles.page}>
             <main className={styles.main}>
-                <div className="vault creation">
-                    <h1>Vault Decryption</h1>
-                    {state?.result?.error && (
-                        <Alert type={"danger"} code={state.result.code} message={state.result.error} />
-                    )}
-                    {state?.result?.data && (
-                        <Alert type={"success"} code={state.result.data.code} message={state.result.data.msg} />
-                    )}
-                    <div className="divider"></div>
-                    <form ref={formRef} className="flex-box row">
-                        <div className="flex-box col" style={{ gap: '7px' }}>
-                            <h2>Decryption Key ({vault?.name})</h2><p>To access this vault, you must upload the AES 256 encryption key that is bound to it. This key is required to view the vault's contents. Once inside the vault, each value must be manually decrypted using the provided key.</p>
-                            <label htmlFor="key-upload" className="custom-file-upload">
-                                {fileName ? fileName : 'Upload AES 256 Key'}
-                            </label>
-                            <input
-                                id="key-upload"
-                                ref={keyUploadRef}
-                                name="key-upload"
-                                type="file"
-                                accept=".aes"
-                                onChange={handleFileChange}
-                            />
-                        </div>
-                    </form>
-                    <div className="divider" style={{ marginTop: '11px', marginBottom: '11px' }}></div>
-                    <div className="actions">
-                        <button type="button" onClick={handleSubmit}>Enter</button>
-                        <button onClick={() => { window.location.href = "/" }}>Cancel</button>
+                {loading ? (
+                    <div className="loading">
+                        <div className="loader"></div>
                     </div>
-                </div>
+                ) : (
+                    <div className="vault creation">
+                        <h1>Vault Decryption</h1>
+                        {state?.result?.error && (
+                            <Alert type={"danger"} code={state.result.code} message={state.result.error} />
+                        )}
+                        {state?.result?.data && (
+                            <Alert type={"success"} code={state.result.data.code} message={state.result.data.msg} />
+                        )}
+                        <div className="divider"></div>
+                        <form ref={formRef} className="flex-box row">
+                            <div className="flex-box col" style={{ gap: '7px' }}>
+                                <h2>Decryption Key ({vault?.name})</h2>
+                                <p>To access this vault, you must upload the AES 256 encryption key that is bound to it. This key is required to view the vault's contents. Once inside the vault, each value must be manually decrypted using the provided key.</p>
+                                <label htmlFor="key-upload" className="custom-file-upload">
+                                    {fileName ? fileName : 'Upload AES 256 Key'}
+                                </label>
+                                <input
+                                    id="key-upload"
+                                    ref={keyUploadRef}
+                                    name="key-upload"
+                                    type="file"
+                                    accept=".aes"
+                                    onChange={handleFileChange}
+                                />
+                            </div>
+                        </form>
+                        <div className="divider" style={{ marginTop: '11px', marginBottom: '11px' }}></div>
+                        <div className="actions">
+                            <button type="button" onClick={handleSubmit}>Enter</button>
+                            <button onClick={() => { window.location.href = "/" }}>Cancel</button>
+                        </div>
+                    </div>
+                )}
             </main>
         </div>
     );
