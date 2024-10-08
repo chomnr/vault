e730a0064decf3f32558e101765381c8d888912d
chomnr <zeljkovranjes@outlook.com>
2024-10-08 17:33:38 -0400

    testing

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -25,18 +25,22 @@ export default function Home() {
         <div className={styles.page}>
             <main className={styles.main}>
                 <div className="vault deletion">
-                    <h1>Warning: Deleting Vault</h1>
+                    <h1>Credential Creation</h1>
                     <div className="divider" style={{ marginBottom: '7px' }}></div>
                     <form className="flex-box row">
                         <div className="flex-box col" style={{ gap: '7px' }}>
                             <p>Are you sure you want to delete this vault? Once deleted, all associated credentials and data within the vault will be permanently removed and cannot be recovered. This action cannot be undone. Please ensure you have backed up any important information before proceeding.</p>
-                            <h2>Please type <i>cool vault name heroes here</i> to confirm</h2>
-                            <Input placeholder="Vault Name" type={"search"} />
+                            <h2>Credential Type</h2>
+                            <p>Credential Type can be any value. There are no set values for it.</p>
+                            <Input placeholder="Credential Type (Ex: Google, Microsoft, Discord)" type={"text"} />
+                            <h2>Credential Name</h2>
+                            <p>The name of or description of your credential.</p>
+                            <Input placeholder="Credential Name" type={"text"} />
                         </div>
                     </form>
                     <div className="divider" style={{ marginTop: '11px', marginBottom: '11px' }}></div>
                     <div className="actions" style={{display: "flex", gap: '5px'}}>
-                        <button type="button">Delete</button>
+                        <button type="button">Save</button>
                         <button onClick={() => { window.location.href = "/" }}>Cancel</button>
                     </div>
                 </div>
