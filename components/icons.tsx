ef7633a705d93c1f30787e3edba727a9f7503abd
chomnr <zeljkovranjes@outlook.com>
2024-10-08 02:38:17 -0400

    added trashcan icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -29,6 +29,28 @@ export const CheckMark: React.FC<IconSvgProps> = ({
   </svg>
 );
 
+
+export const Trashcan: React.FC<IconSvgProps> = ({
+  size = 42,
+  width = size,
+  height = size,
+  ...props
+}) => (
+  <svg
+    fill="#ffffff"
+    width={width}
+    height={height}
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"
+      fill="#ffffff"
+    />
+  </svg>
+);
+
 export const DiscordIcon: React.FC<IconSvgProps> = ({
   size = 24,
   width,
