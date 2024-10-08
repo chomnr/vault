d29d57c4bbe18006b9d6b67eb31c401292556391
chomnr <zeljkovranjes@outlook.com>
2024-10-08 12:17:48 -0400

    updated redirect

--- app/vault/delete/page.tsx
+++ app/vault/delete/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
                         <div className="divider" style={{ marginTop: '11px', marginBottom: '11px' }}></div>
                         <div className="actions" style={{ display: "flex", gap: '5px' }}>
                             <button type="submit" onClick={handleDelete}>Delete</button>
-                            <button onClick={() => { window.location.href = "/" }}>Cancel</button>
+                            <button onClick={() => { window.location.href = "/vault" }}>Cancel</button>
                         </div>
                     </div>
                 )}
