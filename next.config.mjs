ee96c8079e68802d0fa40b7b789b95ca62c30e8f
chomnr <zeljkovranjes@outlook.com>
2024-10-09 00:44:02 -0400

    comment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false // stops api/vaults from getting twiced.
 };
 
 export default nextConfig;
